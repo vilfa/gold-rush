000a050eacf918cd05d5faac1fe1159152fcad33
Luka Vilfan <vilfan.luka@gmail.com>
2020-09-06 18:33:17 +0200

    colorful triangle (⌐■_■)

--- game/main.cpp
+++ game/main.cpp
@@ -12,23 +12,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderOrangeSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
 "}\n";
 
-const char* fragmentShaderYellowSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(0.75f, 0.6f, 0.2f, 1.0f);"
+"	FragColor = vec4(ourColor, 1.0);\n"
 "}\n";
 
 int main() 
@@ -70,13 +67,13 @@ int main()
 
 	/*----- GEOMETRY -----*/
 	/*
-	* create VAO to store different VBOs, EBO to reuse vertices
+	* create VAO (Vertex Array Object) to store different VBOs, EBO to reuse vertices
 	* you can bind all different kinds of buffers to the VAO!
 	*/
 	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
@@ -84,10 +81,10 @@ int main()
 	* create triangle vertices and bind the Vertex Buffer Object (VBO)
 	*/
 	float vertices[] = {
-		 0.5f, 0.5f, 0.0f,	// top r
-		 0.5f, -0.5f, 0.0f, // bottom r
-		-0.5f, -0.5f, 0.0f, // bottom l
-		-0.5f, 0.5f, 0.0f,	// top l
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+	   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -95,27 +92,26 @@ int main()
 	/*
 	* create element buffer object (EBO) to easily reuse vertices
 	*/
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3,
+	/*unsigned int indices[] = {
+		0, 1, 2,
 	};
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
 
 	/*
 	* tell theGL how to interperet the vertex data VBO
 	* (each value is 4B/32b float)
 	*/
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-		
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	/*----- SHADERS -----*/
 	/*
 	* create, compile, check compile of vertex shader
 	*/
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	checkShaderCompile(&vertexShader, "VERTEX");
@@ -123,38 +119,26 @@ int main()
 	/*
 	* create, compile, check compile of fragment shader
 	*/
-	unsigned int fragmentShaders[2];
-	fragmentShaders[0] = glCreateShader(GL_FRAGMENT_SHADER);
-	fragmentShaders[1] = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShaders[0], 1, &fragmentShaderOrangeSource, NULL);
-	glShaderSource(fragmentShaders[1], 1, &fragmentShaderYellowSource, NULL);
-	glCompileShader(fragmentShaders[0]);
-	glCompileShader(fragmentShaders[1]);
-	checkShaderCompile(&fragmentShaders[0], "FRAGMENT");
-	checkShaderCompile(&fragmentShaders[1], "FRAGMENT");
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShaderCompile(&fragmentShader, "FRAGMENT");
 
 	/*
 	* create shader program to link and use vertex and fragment shader 
 	* (connect vertex outputs to fragment inputs, fragment to...)
 	*/
-	unsigned int shaderPrograms[2];
-	shaderPrograms[0] = glCreateProgram();
-	shaderPrograms[1] = glCreateProgram();
-	glAttachShader(shaderPrograms[0], vertexShader);
-	glAttachShader(shaderPrograms[0], fragmentShaders[0]);
-	glAttachShader(shaderPrograms[1], vertexShader);
-	glAttachShader(shaderPrograms[1], fragmentShaders[1]);
-	glLinkProgram(shaderPrograms[0]);
-	glLinkProgram(shaderPrograms[1]);
-	checkShaderProgramLink(&shaderPrograms[0]);
-	checkShaderProgramLink(&shaderPrograms[1]);
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	checkShaderProgramLink(&shaderProgram);
 
 	/*
 	* delete shader after linking them as we don't need them any longer
 	*/
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShaders[0]);
-	glDeleteShader(fragmentShaders[1]);
+	glDeleteShader(fragmentShader);
 
 	/*----- RENDER -----*/
 	/* 
@@ -171,10 +155,9 @@ int main()
 		// rendering commands
 		glClearColor(.2f, .3f, .3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe mode (delete or use GL_FILL for normal)
-		glUseProgram(shaderPrograms[1]);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -183,9 +166,8 @@ int main()
 
 	glDeleteBuffers(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderPrograms[0]);
-	glDeleteProgram(shaderPrograms[1]);
+	//glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 	return 0;
 }
@@ -212,7 +194,7 @@ void checkShaderCompile(GLuint* vertexShader, const char* type)
 	if (!success)
 	{
 		glGetShaderInfoLog(*vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::" << *type << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+		std::cout << "ERROR::SHADER::" << type << "::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 }
 
